d2a2e6e63634b9260e3d9ea4beb89db2f483d04c
Rodrigo Daros <rodrigo.conspena@gmail.com>
2021-09-03 14:09:48 -0300

    Ajustes.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const app = express();
 const port = 3456;
 const timeout = 1000 * 60 * 3; // 3 minutes
+const apikey = process.env.HTTP_EXECUTOR_KEY ?? 'notsecret';
 
 let commands = [];
 
@@ -15,18 +16,32 @@ function readCommands(error, content) {
 fs.readFile('commands.json', readCommands);
 
 app.get('/exec/:command', (req, res) => {
-  const commandName = req.params.command;
-  const commandObj = commands.find((x) => x.name == commandName);
-  const command = commandObj.command;
+  const key = req.query.k ?? 'notsecret';
 
-  try {
-    execSync(command, { timeout: timeout });
-  } catch (error) {
-    res.status(500).send('Error!');
+  if (key != apikey) {
+    throw 'Invalid API key.';
   }
 
-  res.status(200).send('Command executed!');
+  const commandName = req.params.command;
+  const command = commands.find((x) => x.name == commandName);
+
+  console.log('Executing command:');
+  console.log({ command });
+
+  const options = { stdio: 'inherit', timeout: timeout, cwd: command.workdir };
+
+  try {
+    execSync(command.command, options);
+  } catch (error) {
+    console.error(error);
+    throw error.message;
+  }
+
+  console.log('Sucess!');
+  res.send('Command executed!');
 });
+
+app.get('/health', (_, res) => res.send("I'm fine!"));
 
 app.listen(port, () => {
   console.log(`Listening at http://0.0.0.0:${port}`);
